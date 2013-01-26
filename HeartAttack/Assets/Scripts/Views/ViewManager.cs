5264951dc7d6bbd95c7d80c6a8c2efffc9cd1b0b
Aaron Jones <aaron@Aarons-MacBook-Pro.local>
2013-01-25 20:32:45 -0800

    sprites and views

---
+++ HeartAttack/Assets/Scripts/Views/ViewManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class ViewManager : MonoBehaviour {
+	public GameObject centerAnchor;
+	public GameObject upperRightAnchor;
+	public GameObject upperLeftAnchor;
+
+	void Start() {
+		this.positionAnchors();
+	}
+
+	void positionAnchors() {
+		if (centerAnchor) {
+			centerAnchor.transform.localPosition = Vector3.zero;
+		}
+		if (upperRightAnchor) {
+			upperRightAnchor.transform.localPosition = new Vector3(Screen.width, Screen.height,0);
+		}
+		if (upperLeftAnchor) {
+			upperLeftAnchor.transform.localPosition = new Vector3(-Screen.width, Screen.height,0);
+		}
+	}
+	
+}
